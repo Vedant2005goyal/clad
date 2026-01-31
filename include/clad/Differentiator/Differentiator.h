258c8eb3e6bcd9535348ae6a2f2be3ea0ec83f43
Vedant2005goyal <goyalvedant2005@gmail.com>
2026-01-31 11:04:39 +0530

    Added multilayer storage featrue to the tape now when the RAM gets full the data can be offloaded to the Disk similar to the LRU

--- include/clad/Differentiator/Differentiator.h
+++ include/clad/Differentiator/Differentiator.h
@@ -59,90 +59,107 @@ inline CUDA_HOST_DEVICE unsigned int GetLength(const char* code) {
 
 /// Tape type used for storing values in reverse-mode AD inside loops.
 template <typename T, std::size_t SBO_SIZE = 64, std::size_t SLAB_SIZE = 1024,
-          bool is_multithread = false>
-using tape = tape_impl<T, SBO_SIZE, SLAB_SIZE, is_multithread>;
+          bool is_multithread = false, bool DiskOffload = false>
+using tape = tape_impl<T, SBO_SIZE, SLAB_SIZE, is_multithread, DiskOffload>;
 
 /// Add value to the end of the tape, return the same value.
 template <typename T, std::size_t SBO_SIZE = 64, std::size_t SLAB_SIZE = 1024,
-          typename... ArgsT>
-CUDA_HOST_DEVICE T& push(tape<T, SBO_SIZE, SLAB_SIZE>& to, ArgsT... val) {
+          bool DiskOffload = false, typename... ArgsT>
+CUDA_HOST_DEVICE T&
+push(tape<T, SBO_SIZE, SLAB_SIZE, /*is_multithread=*/false, DiskOffload>& to,
+     ArgsT... val) {
   to.emplace_back(std::forward<ArgsT>(val)...);
   return to.back();
 }
 
 /// A specialization for C arrays
 template <typename T, typename U, size_t N, std::size_t SBO_SIZE = 64,
-          std::size_t SLAB_SIZE = 1024>
-CUDA_HOST_DEVICE void push(tape<T[N], SBO_SIZE, SLAB_SIZE>& to, const U& val) {
+          std::size_t SLAB_SIZE = 1024, bool DiskOffload = false>
+CUDA_HOST_DEVICE void
+push(tape<T[N], SBO_SIZE, SLAB_SIZE, /*is_multithread=*/false, DiskOffload>& to,
+     const U& val) {
   to.emplace_back();
   std::copy(std::begin(val), std::end(val), std::begin(to.back()));
 }
 
   /// Remove the last value from the tape, return it.
-  template <typename T, std::size_t SBO_SIZE = 64, std::size_t SLAB_SIZE = 1024>
-  CUDA_HOST_DEVICE T pop(tape<T, SBO_SIZE, SLAB_SIZE>& to) {
-    T val = std::move(to.back());
-    to.pop_back();
-    return val;
-  }
+template <typename T, std::size_t SBO_SIZE = 64, std::size_t SLAB_SIZE = 1024,
+          bool DiskOffload = false>
+CUDA_HOST_DEVICE T
+pop(tape<T, SBO_SIZE, SLAB_SIZE, /*is_multithread=*/false, DiskOffload>& to) {
+  T val = std::move(to.back());
+  to.pop_back();
+  return val;
+}
 
   /// A specialization for C arrays
-  template <typename T, std::size_t N, std::size_t SBO_SIZE = 64,
-            std::size_t SLAB_SIZE = 1024>
-  CUDA_HOST_DEVICE void pop(tape<T[N], SBO_SIZE, SLAB_SIZE>& to) {
-    to.pop_back();
-  }
+template <typename T, std::size_t N, std::size_t SBO_SIZE = 64,
+          std::size_t SLAB_SIZE = 1024, bool DiskOffload = false>
+CUDA_HOST_DEVICE void pop(tape<T[N], SBO_SIZE, SLAB_SIZE,
+                               /*is_multithread=*/false, DiskOffload>& to) {
+  to.pop_back();
+}
 
   /// Access return the last value in the tape.
-  template <typename T> CUDA_HOST_DEVICE T& back(tape<T>& of) {
-    return of.back();
-  }
+template <typename T, std::size_t SBO_SIZE = 64, std::size_t SLAB_SIZE = 1024,
+          bool DiskOffload = false>
+CUDA_HOST_DEVICE T&
+back(tape<T, SBO_SIZE, SLAB_SIZE, /*is_multithread=*/false, DiskOffload>& of) {
+  return of.back();
+}
 
   /// Thread safe tape access functions with mutex locking mechanism
+/// Thread safe tape access functions with mutex locking mechanism
 #ifndef __CUDACC__
-  /// Add value to the end of the tape, return the same value.
-  template <typename T, std::size_t SBO_SIZE = 64, std::size_t SLAB_SIZE = 1024,
-            typename... ArgsT>
-  T push(tape<T, SBO_SIZE, SLAB_SIZE, /*is_multithreaded=*/true>& to,
-         ArgsT&&... val) {
-    std::lock_guard<std::mutex> lock(to.mutex());
-    to.emplace_back(std::forward<ArgsT>(val)...);
-    return to.back();
-  }
+/// Add value to the end of the tape, return the same value.
+template <typename T, std::size_t SBO_SIZE = 64, std::size_t SLAB_SIZE = 1024,
+          bool DiskOffload = false, typename... ArgsT>
+T push(tape<T, SBO_SIZE, SLAB_SIZE, /*is_multithreaded=*/true, DiskOffload>& to,
+       ArgsT&&... val) {
+  std::lock_guard<std::mutex> lock(to.mutex());
+  to.emplace_back(std::forward<ArgsT>(val)...);
+  return to.back();
+}
 
   /// A specialization for C arrays
-  template <typename T, typename U, size_t N, std::size_t SBO_SIZE = 64,
-            std::size_t SLAB_SIZE = 1024>
-  void push(tape<T[N], SBO_SIZE, SLAB_SIZE, /*is_multithreaded=*/true>& to,
-            const U& val) {
-    std::lock_guard<std::mutex> lock(to.mutex());
-    to.emplace_back();
-    std::copy(std::begin(val), std::end(val), std::begin(to.back()));
-  }
+template <typename T, typename U, size_t N, std::size_t SBO_SIZE = 64,
+          std::size_t SLAB_SIZE = 1024, bool DiskOffload = false>
+void push(
+    tape<T[N], SBO_SIZE, SLAB_SIZE, /*is_multithreaded=*/true, DiskOffload>& to,
+    const U& val) {
+  std::lock_guard<std::mutex> lock(to.mutex());
+  to.emplace_back();
+  std::copy(std::begin(val), std::end(val), std::begin(to.back()));
+}
 
   /// Remove the last value from the tape, return it.
-  template <typename T, std::size_t SBO_SIZE = 64, std::size_t SLAB_SIZE = 1024>
-  T pop(tape<T, SBO_SIZE, SLAB_SIZE, /*is_multithreaded=*/true>& to) {
-    std::lock_guard<std::mutex> lock(to.mutex());
-    T val = std::move(to.back());
-    to.pop_back();
-    return val;
-  }
+template <typename T, std::size_t SBO_SIZE = 64, std::size_t SLAB_SIZE = 1024,
+          bool DiskOffload = false>
+T pop(
+    tape<T, SBO_SIZE, SLAB_SIZE, /*is_multithreaded=*/true, DiskOffload>& to) {
+  std::lock_guard<std::mutex> lock(to.mutex());
+  T val = std::move(to.back());
+  to.pop_back();
+  return val;
+}
 
   /// A specialization for C arrays
-  template <typename T, std::size_t N, std::size_t SBO_SIZE = 64,
-            std::size_t SLAB_SIZE = 1024>
-  void pop(tape<T[N], SBO_SIZE, SLAB_SIZE, /*is_multithreaded=*/true>& to) {
-    std::lock_guard<std::mutex> lock(to.mutex());
-    to.pop_back();
-  }
+template <typename T, std::size_t N, std::size_t SBO_SIZE = 64,
+          std::size_t SLAB_SIZE = 1024, bool DiskOffload = false>
+void pop(tape<T[N], SBO_SIZE, SLAB_SIZE, /*is_multithreaded=*/true,
+              DiskOffload>& to) {
+  std::lock_guard<std::mutex> lock(to.mutex());
+  to.pop_back();
+}
 
   /// Access return the last value in the tape.
-  template <typename T, std::size_t SBO_SIZE = 64, std::size_t SLAB_SIZE = 1024>
-  T& back(tape<T, SBO_SIZE, SLAB_SIZE, /*is_multithreaded=*/true>& of) {
-    std::lock_guard<std::mutex> lock(of.mutex());
-    return of.back();
-  }
+template <typename T, std::size_t SBO_SIZE = 64, std::size_t SLAB_SIZE = 1024,
+          bool DiskOffload = false>
+T& back(
+    tape<T, SBO_SIZE, SLAB_SIZE, /*is_multithreaded=*/true, DiskOffload>& of) {
+  std::lock_guard<std::mutex> lock(of.mutex());
+  return of.back();
+}
 #endif
 
   /// The purpose of this function is to initialize adjoints
--- include/clad/Differentiator/Tape.h
+++ include/clad/Differentiator/Tape.h
@@ -4,10 +4,14 @@
 #include "clad/Differentiator/CladConfig.h"
 #include <cassert>
 #include <cstddef>
+#include <cstdint>
 #include <cstdio>
+#include <fstream>
+#include <ios>
 #include <iterator>
 #include <memory>
 #include <new>
+#include <string>
 #include <type_traits>
 #include <utility>
 #ifndef __CUDACC__
@@ -16,19 +20,66 @@
 
 namespace clad {
 
+namespace detail {
+
+template <typename T, std::size_t SLAB_SIZE> struct DiskManager {
+#ifndef __CUDA_ARCH__
+  std::fstream file;
+  std::string filename;
+  DiskManager() {
+    filename = "clad_tape_" + std::to_string((uintptr_t)this) + ".tmp";
+    file.open(filename, std::ios::in | std::ios::out | std::ios::binary |
+                            std::ios::trunc);
+  }
+  ~DiskManager() {
+    if (file.is_open())
+      file.close();
+    std::remove(filename.c_str());
+  }
+  std::size_t write_slab(const T* data) {
+    file.seekp(0, std::ios::end);
+    std::size_t offset = file.tellp();
+    const void* raw_data = static_cast<const void*>(data);
+    file.write(static_cast<const char*>(raw_data), SLAB_SIZE * sizeof(T));
+    return offset;
+  }
+  void read_slab(T* dest, std::size_t offset) {
+    file.seekg(offset, std::ios::beg);
+    void* raw_dest = static_cast<void*>(dest);
+    file.read(static_cast<char*>(raw_dest), SLAB_SIZE * sizeof(T));
+  }
+#else
+  CUDA_HOST_DEVICE DiskManager() {}
+  CUDA_HOST_DEVICE ~DiskManager() {}
+  CUDA_HOST_DEVICE std::size_t write_slab(const T* data) { return 0; }
+  CUDA_HOST_DEVICE void read_slab(T* dest, std::size_t offset) {}
+#endif
+};
+
+template <bool Enabled, typename T, std::size_t SLAB_SIZE>
+struct OffloadStorage;
+
+template <typename T, std::size_t SLAB_SIZE>
+struct OffloadStorage<false, T, SLAB_SIZE> {};
+
+template <typename T, std::size_t SLAB_SIZE>
+struct OffloadStorage<true, T, SLAB_SIZE> {
+  std::unique_ptr<DiskManager<T, SLAB_SIZE>> m_DiskManager;
+  std::size_t m_ActiveSlabs = 0;
+  std::size_t m_MaxRamSlabs = 1000;
+};
+
+} // namespace detail
+
 template <typename T, std::size_t SBO_SIZE, std::size_t SLAB_SIZE,
-          bool is_multithread>
+          bool is_multithread, bool DiskOffload>
 class tape_impl;
 
-/// A forward iterator for traversing elements in `clad::tape_impl`.
-/// This iterator supports standard forward iteration operations, including:
-/// - Dereferencing (`*`, `->`)
-/// - Increment (`++`)
-/// - Equality and inequality comparisons
 template <typename T, std::size_t SBO_SIZE = 64, std::size_t SLAB_SIZE = 1024,
-          bool is_multithread = false>
+          bool is_multithread = false, bool DiskOffload = false>
 class tape_iterator {
-  using tape_t = clad::tape_impl<T, SBO_SIZE, SLAB_SIZE, is_multithread>;
+  using tape_t =
+      clad::tape_impl<T, SBO_SIZE, SLAB_SIZE, is_multithread, DiskOffload>;
   tape_t* m_tape;
   std::size_t m_index;
 
@@ -44,7 +95,6 @@ public:
       : m_tape(tape), m_index(index) {}
 
   CUDA_HOST_DEVICE reference operator*() const { return (*m_tape)[m_index]; }
-
   CUDA_HOST_DEVICE pointer operator->() const { return &(*m_tape)[m_index]; }
 
   CUDA_HOST_DEVICE tape_iterator& operator++() {
@@ -67,23 +117,12 @@ public:
   }
 };
 
-/// A dynamic slab-based vector-like container with Small Buffer Optimization
-/// (SBO), primarily used for storing values in reverse-mode AD. Stores elements
-/// in a static buffer first, then falls back to dynamically allocated linked
-/// slabs if capacity exceeds SBO.
 template <typename T, std::size_t SBO_SIZE = 64, std::size_t SLAB_SIZE = 1024,
-          bool is_multithread = false>
+          bool is_multithread = false, bool DiskOffload = false>
 class tape_impl {
-  /// A block of contiguous storage allocated dynamically when SBO capacity is
-  /// exceeded.
-  struct Slab {
-    // std::aligned_storage_t<sizeof(T), alignof(T)> raw_data[SLAB_SIZE];
-    // For now use the implementation below as above implementation is not
-    // supported by c++11
+  struct RAMStorage {
     alignas(T) char raw_data[SLAB_SIZE * sizeof(T)];
-    Slab* prev;
-    Slab* next;
-    CUDA_HOST_DEVICE Slab() : prev(nullptr), next(nullptr) {}
+    CUDA_HOST_DEVICE RAMStorage() {}
     CUDA_HOST_DEVICE T* elements() {
 #if __cplusplus >= 201703L
       return std::launder(reinterpret_cast<T*>(raw_data));
@@ -93,9 +132,43 @@ class tape_impl {
     }
   };
 
-  // std::aligned_storage_t<sizeof(T), alignof(T)> m_static_buffer[SBO_SIZE];
-  // For now use the implementation below as above implementation is not
-  // supported by c++11
+  struct DiskStorage {
+    T* data_ptr = nullptr;
+    bool is_on_disk = false;
+    std::size_t disk_offset = 0;
+
+    CUDA_HOST_DEVICE DiskStorage() { allocate(); }
+    CUDA_HOST_DEVICE ~DiskStorage() { deallocate(); }
+
+    DiskStorage(const DiskStorage&) = delete;
+    DiskStorage& operator=(const DiskStorage&) = delete;
+
+    void allocate() {
+      if (!data_ptr)
+        data_ptr = static_cast<T*>(::operator new(SLAB_SIZE * sizeof(T)));
+    }
+
+    void deallocate() {
+      if (data_ptr) {
+        ::operator delete(data_ptr);
+        data_ptr = nullptr;
+      }
+    }
+
+    CUDA_HOST_DEVICE T* elements() { return data_ptr; }
+  };
+
+  using SlabBase = std::conditional_t<DiskOffload, DiskStorage, RAMStorage>;
+
+public:
+  struct Slab : public SlabBase {
+    Slab* prev;
+    Slab* next;
+
+    CUDA_HOST_DEVICE Slab() : prev(nullptr), next(nullptr) {}
+  };
+
+private:
   alignas(T) char m_static_buffer[SBO_SIZE * sizeof(T)];
 
   Slab* m_head = nullptr;
@@ -106,6 +179,8 @@ class tape_impl {
 #ifndef __CUDACC__
   mutable std::mutex m_TapeMutex;
 #endif
+
+  detail::OffloadStorage<DiskOffload, T, SLAB_SIZE> m_Offload;
 
   CUDA_HOST_DEVICE T* sbo_elements() {
 #if __cplusplus >= 201703L
@@ -123,6 +198,55 @@ class tape_impl {
 #endif
   }
 
+  inline __attribute__((always_inline)) void check_and_evict(std::true_type) {
+    if (m_Offload.m_ActiveSlabs >= m_Offload.m_MaxRamSlabs) {
+      Slab* candidate = m_head;
+      while (candidate && (candidate->is_on_disk || candidate == m_tail))
+        candidate = candidate->next;
+      if (candidate) {
+        if (!m_Offload.m_DiskManager)
+          m_Offload.m_DiskManager.reset(
+              new detail::DiskManager<T, SLAB_SIZE>());
+        candidate->disk_offset =
+            m_Offload.m_DiskManager->write_slab(candidate->elements());
+        candidate->deallocate();
+        candidate->is_on_disk = true;
+        m_Offload.m_ActiveSlabs--;
+      }
+    }
+  }
+  inline __attribute__((always_inline)) void check_and_evict(std::false_type) {}
+
+  inline __attribute__((always_inline)) void ensure_loaded(Slab* slab,
+                                                           std::true_type) {
+    if (slab && slab->is_on_disk) {
+      if (m_Offload.m_ActiveSlabs >= m_Offload.m_MaxRamSlabs) {
+        Slab* v = m_head;
+        while (v) {
+          if (!v->is_on_disk && v != slab) {
+            v->disk_offset = m_Offload.m_DiskManager->write_slab(v->elements());
+            v->deallocate();
+            v->is_on_disk = true;
+            m_Offload.m_ActiveSlabs--;
+            break;
+          }
+          v = v->next;
+        }
+      }
+      slab->allocate();
+      m_Offload.m_DiskManager->read_slab(slab->elements(), slab->disk_offset);
+      slab->is_on_disk = false;
+      m_Offload.m_ActiveSlabs++;
+    }
+  }
+  inline __attribute__((always_inline)) void ensure_loaded(Slab* slab,
+                                                           std::false_type) {}
+
+  inline __attribute__((always_inline)) void track_new_slab(std::true_type) {
+    m_Offload.m_ActiveSlabs++;
+  }
+  inline __attribute__((always_inline)) void track_new_slab(std::false_type) {}
+
 public:
   using reference = T&;
   using const_reference = const T&;
@@ -131,9 +255,10 @@ public:
   using size_type = std::size_t;
   using difference_type = std::ptrdiff_t;
   using value_type = T;
-  using iterator = tape_iterator<T, SBO_SIZE, SLAB_SIZE, is_multithread>;
+  using iterator =
+      tape_iterator<T, SBO_SIZE, SLAB_SIZE, is_multithread, DiskOffload>;
   using const_iterator =
-      tape_iterator<const T, SBO_SIZE, SLAB_SIZE, is_multithread>;
+      tape_iterator<const T, SBO_SIZE, SLAB_SIZE, is_multithread, DiskOffload>;
 
 #ifndef __CUDACC__
   std::mutex& mutex() const { return m_TapeMutex; }
@@ -141,21 +266,27 @@ public:
 
   CUDA_HOST_DEVICE tape_impl() = default;
 
+  tape_impl(const tape_impl&) = delete;
+  tape_impl& operator=(const tape_impl&) = delete;
+  tape_impl(tape_impl&&) = delete;
+  tape_impl& operator=(tape_impl&&) = delete;
+
   CUDA_HOST_DEVICE ~tape_impl() { clear(); }
 
-  /// Add new value of type T constructed from args to the end of the tape.
   template <typename... ArgsT>
   CUDA_HOST_DEVICE void emplace_back(ArgsT&&... args) {
     if (m_size < SBO_SIZE) {
-      // Store in SBO buffer
       ::new (const_cast<void*>(static_cast<const volatile void*>(
           sbo_elements() + m_size))) T(std::forward<ArgsT>(args)...);
     } else {
       const auto offset = (m_size - SBO_SIZE) % SLAB_SIZE;
-      // Allocate new slab if required
+
       if (!offset) {
         if (m_size == m_capacity) {
+          check_and_evict(std::integral_constant<bool, DiskOffload>{});
+
           Slab* new_slab = new Slab();
+          track_new_slab(std::integral_constant<bool, DiskOffload>{});
           if (!m_head)
             m_head = new_slab;
           else {
@@ -170,7 +301,7 @@ public:
           m_tail = m_tail->next;
       }
 
-      // Construct element in-place
+      ensure_loaded(m_tail, std::integral_constant<bool, DiskOffload>{});
       ::new (const_cast<void*>(static_cast<const volatile void*>(
           m_tail->elements() + offset))) T(std::forward<ArgsT>(args)...);
     }
@@ -178,36 +309,24 @@ public:
   }
 
   CUDA_HOST_DEVICE std::size_t size() const { return m_size; }
-
   CUDA_HOST_DEVICE iterator begin() { return iterator(this, 0); }
-
   CUDA_HOST_DEVICE const_iterator begin() const {
     return const_iterator(this, 0);
   }
-
   CUDA_HOST_DEVICE iterator end() { return iterator(this, m_size); }
-
   CUDA_HOST_DEVICE const_iterator end() const {
     return const_iterator(this, m_size);
   }
 
-  /// Access last value (must not be empty).
   CUDA_HOST_DEVICE reference back() {
     assert(m_size);
-    std::size_t index = m_size - 1;
-    if (index < SBO_SIZE)
-      return *(sbo_elements() + index);
-    index = (index - SBO_SIZE) % SLAB_SIZE;
-    return *(m_tail->elements() + index);
+    return (*this)[m_size - 1];
   }
 
   CUDA_HOST_DEVICE const_reference back() const {
     assert(m_size);
-    std::size_t index = m_size - 1;
-    if (index < SBO_SIZE)
-      return *(sbo_elements() + index);
-    index = (index - SBO_SIZE) % SLAB_SIZE;
-    return *(m_tail->elements() + index);
+    // NOLINTNEXTLINE(cppcoreguidelines-pro-type-const-cast)
+    return const_cast<tape_impl*>(this)->operator[](m_size - 1);
   }
 
   CUDA_HOST_DEVICE reference operator[](std::size_t i) {
@@ -217,16 +336,18 @@ public:
 
   CUDA_HOST_DEVICE const_reference operator[](std::size_t i) const {
     assert(i < m_size);
-    return *at(i);
+    // NOLINTNEXTLINE(cppcoreguidelines-pro-type-const-cast)
+    return *const_cast<tape_impl*>(this)->at(i);
   }
 
-  /// Remove the last value from the tape.
   CUDA_HOST_DEVICE void pop_back() {
     assert(m_size);
     m_size--;
     if (m_size < SBO_SIZE)
       destroy_element(sbo_elements() + m_size);
     else {
+      ensure_loaded(m_tail, std::integral_constant<bool, DiskOffload>{});
+
       std::size_t offset = (m_size - SBO_SIZE) % SLAB_SIZE;
       destroy_element(m_tail->elements() + offset);
       if (offset == 0) {
@@ -241,20 +362,14 @@ private:
   CUDA_HOST_DEVICE T* at(std::size_t index) {
     if (index < SBO_SIZE)
       return sbo_elements() + index;
-    Slab* slab = m_head;
-    std::size_t idx = (index - SBO_SIZE) / SLAB_SIZE;
-    while (idx--)
-      slab = slab->next;
-    return slab->elements() + ((index - SBO_SIZE) % SLAB_SIZE);
-  }
 
-  CUDA_HOST_DEVICE const T* at(std::size_t index) const {
-    if (index < SBO_SIZE)
-      return sbo_elements() + index;
     Slab* slab = m_head;
     std::size_t idx = (index - SBO_SIZE) / SLAB_SIZE;
     while (idx--)
       slab = slab->next;
+
+    ensure_loaded(slab, std::integral_constant<bool, DiskOffload>{});
+
     return slab->elements() + ((index - SBO_SIZE) % SLAB_SIZE);
   }
 
@@ -280,15 +395,20 @@ private:
   /// Destroys all elements and deallocates slabs
   void clear() {
     std::size_t count = m_size;
-
     for (std::size_t i = 0; i < SBO_SIZE && count > 0; ++i, --count)
       destroy_element(&sbo_elements()[i]);
 
     Slab* slab = m_head;
     while (slab) {
-      T* elems = slab->elements();
-      for (size_t i = 0; i < SLAB_SIZE && count > 0; ++i, --count)
-        destroy_element(elems + i);
+      if constexpr (!DiskOffload) {
+        T* elems = slab->elements();
+        for (size_t i = 0; i < SLAB_SIZE && count > 0; ++i, --count)
+          destroy_element(elems + i);
+      } else {
+        size_t current_slab_count = (count > SLAB_SIZE) ? SLAB_SIZE : count;
+        count -= current_slab_count;
+      }
+
       Slab* tmp = slab;
       slab = slab->next;
       delete tmp;
@@ -298,10 +418,11 @@ private:
     m_tail = nullptr;
     m_size = 0;
     m_capacity = SBO_SIZE;
+    if constexpr (DiskOffload)
+      m_Offload.m_ActiveSlabs = 0;
   }
 
   template <typename ElTy> void destroy_element(ElTy* elem) { elem->~ElTy(); }
-
   template <typename ElTy, size_t N> void destroy_element(ElTy (*arr)[N]) {
     for (size_t i = 0; i < N; ++i)
       (*arr)[i].~ElTy();
